8eb32b035f5ad3e619e3b033b6a947e95d6faedd
Clément Pagnot <clementpagnot@gmail.com>
2016-11-15 16:29:48 +0100

    Scroll animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 var controller = new ScrollMagic.Controller();
 
+var carte = TweenMax.staggerFromTo("#carte", 2, {"margin-left": "-25%"}, {"margin-left": "0%", ease: Linear.easeNone}, 0.15);
 var facture = TweenMax.staggerFromTo(".facture", 2, {"margin-left": "-100%"}, {"margin-left": "0%", ease: Linear.easeNone}, 0.15);
 var cerfa = TweenMax.staggerFromTo(".anime-cerfa", 2, {"margin-left": "100%"}, {"margin-left": "0%", ease: Linear.easeNone}, 0.15);
 var lettre = TweenMax.staggerFromTo("#lettre", 2, {"margin-left": "10%"}, {"margin-left": "55%", ease: Linear.easeNone}, 0.15);
 var bulletin = TweenMax.staggerFromTo("#bulletin", 2, {"margin-top": "-2%"}, {"margin-top": "5%", ease: Linear.easeNone}, 0.15);
 // build scene
-var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 400})
+var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400})
+				.setTween(carte)
+				.addIndicators({name: "staggering"}) // add indicators (requires plugin)
+				.addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 400})
 				.setTween(facture)
 				.addIndicators({name: "staggering"}) // add indicators (requires plugin)
 				.addTo(controller);
